8a7e6543d85db0d34cf300a271d0d45c3a012800
tanshika225 <jstanshika1402@gmail.com>
2026-05-13 12:33:37 +0530

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,46 @@
+package task3;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server {
+
+    private static final int PORT = 5000;
+    private static Set<ClientHandler> clientHandlers = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        System.out.println("Server Started...");
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                System.out.println("New client connected");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+                clientHandlers.add(clientHandler);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void broadcastMessage(String message, ClientHandler excludeUser) {
+        for (ClientHandler client : clientHandlers) {
+            if (client != excludeUser) {
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    public static void removeClient(ClientHandler clientHandler) {
+        clientHandlers.remove(clientHandler);
+    }
+}
